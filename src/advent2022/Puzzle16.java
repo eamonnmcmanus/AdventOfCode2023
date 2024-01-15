797f7259b1a77701e1746329eaedb98db9616d12
Éamonn McManus <emcmanus@google.com>
2024-01-15 10:50:59 -0800

    Vastly improved solution for Day 16 of 2022, using an idea from Martijn Pieters.

--- src/advent2022/Puzzle16.java
+++ src/advent2022/Puzzle16.java
@@ -1,7 +1,7 @@
 package advent2022;
 
 import static com.google.common.base.Preconditions.checkArgument;
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
+import static java.lang.Long.max;
 
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
@@ -13,12 +13,10 @@ import com.google.common.io.CharStreams;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.Callable;
 import java.util.regex.Matcher;
@@ -61,17 +59,45 @@ public class Puzzle16 {
 
   private final Graph<Valve> graph;
   private final Valve start;
-  private final List<Valve> valves;
+  private final List<Valve> nonZeroValves;
 
-  Puzzle16(Graph<Valve> graph, List<Valve> valves) {
+  Puzzle16(Graph<Valve> graph, List<Valve> nonZeroValves) {
     this.graph = graph;
-    this.valves = valves;
+    this.nonZeroValves = nonZeroValves;
     this.start = graph.nodes().stream().filter(v -> v.name.equals("AA")).findFirst().get();
   }
 
   private void part1(String name) {
-    Map<State, Long> currentStates = Map.of(new State(start, start, new ValveSet()), 0L);
-    for (int i = 1; i <= 30; i++) {
+    Map<ValveSet, Long> bestSets = bestSets(30);
+    long max = Collections.max(bestSets.values());
+    System.out.println(STR."Max for \{name} part 1 is \{max}");
+  }
+
+  // For part 2, we compute all the possible sets of valves that can be open after 26 minutes, and
+  // we look for the pair of sets from within those that have no intersection and that have the
+  // highest combined total. That corresponds to the elephant and us each opening a distinct set
+  // of valves. I got this idea from Martijn Pieters here:
+  // https://www.reddit.com/r/adventofcode/comments/zo21au/2022_day_16_approaches_and_pitfalls_discussion/
+  // My previous solution was too literal, tracing all possible states of our position and the
+  // elephant's position and the total set of open valves. Even with optimization it took about
+  // 8 minutes to run, versus less than a second for the solution here.
+  private void part2(String name) {
+    Map<ValveSet, Long> bestSets = bestSets(26);
+    long max = 0;
+    for (ValveSet set1 : bestSets.keySet()) {
+      for (ValveSet set2 : bestSets.keySet()) {
+        if (set1.disjoint(set2)) {
+          long total = bestSets.get(set1) + bestSets.get(set2);
+          max = max(max, total);
+        }
+      }
+    }
+    System.out.println(STR."Max for \{name} part 2 is \{max}");
+  }
+
+  private Map<ValveSet, Long> bestSets(int steps) {
+    Map<State, Long> currentStates = Map.of(new State(start, new ValveSet()), 0L);
+    for (int i = 1; i <= steps; i++) {
       Map<State, Long> nextStates = new HashMap<>();
       currentStates.forEach(
           (state, total) -> {
@@ -79,109 +105,22 @@ public class Puzzle16 {
 
             // We move.
             for (Valve ourNext : graph.successors(state.ourPos)) {
-              nextStates.merge(new State(ourNext, state.elephantPos, state.open), total + flowRate, Long::max);
+              nextStates.merge(new State(ourNext, state.open), total + flowRate, Long::max);
             }
 
             // We open.
             if (state.ourPos.flowRate > 0) {
-              nextStates.merge(new State(state.ourPos, state.elephantPos, state.open.plus(state.ourPos)), total + flowRate, Long::max);
+              nextStates.merge(new State(state.ourPos, state.open.plus(state.ourPos)), total + flowRate, Long::max);
             }
           });
       currentStates = nextStates;
     }
-    long max = currentStates.values().stream().max(Comparator.naturalOrder()).get();
-    System.out.println(STR."Max for \{name} part 1 is \{max}");
+    Map<ValveSet, Long> result = new HashMap<>();
+    currentStates.forEach((state, total) -> result.merge(state.open, total, Long::max));
+    return result;
   }
 
-  // This stupidly literal implementation takes about 8 minutes to run. Looking at forum discussion
-  // https://www.reddit.com/r/adventofcode/comments/zo21au/2022_day_16_approaches_and_pitfalls_discussion/
-  // there are a couple of fairly easy things that could be done to make it much better if I had the patience.
-  // First, we can eliminate valves from the graph that have zero flowrate, and instead link interesting
-  // valves with edges that have a cost equal to the shortest path between them. We'd then have to run
-  // through every path in the graph rather than using the Dynamic Programming approach here, but many
-  // paths would be eliminated because the overall time would be too long. (This is not obviously true
-  // but apparently is the case for the problem graph.)
-  // Second, we could have the algorithm determine the best total flowrate for each set of valves
-  // operated by a *single* agent, then find a combination of two sets from this result that are
-  // complementary and that have the highest total. The two sets represent valves opened by us and
-  // by the elephant.
-  private void part2(String name) {
-    Map<State, Long> currentStates = Map.of(new State(start, start, new ValveSet()), 0L);
-    for (int i = 1; i <= 26; i++) {
-      System.out.println(STR."Step \{i}, size \{currentStates.size()}");
-      Map<State, Long> nextStates = new HashMap<>();
-      currentStates.forEach(
-          (state, total) -> {
-            long flowRate = state.open.flowRate();
-
-            Set<State> newStates = new HashSet<>();
-
-            // We move.
-            for (Valve ourNext : graph.successors(state.ourPos)) {
-              // We move and elephant moves.
-              for (Valve elephantNext : graph.successors(state.elephantPos)) {
-                newStates.add(new State(ourNext, elephantNext, state.open));
-              }
-              // We move and elephant opens.
-              newStates.add(new State(ourNext, state.elephantPos, state.open.plus(state.elephantPos)));
-            }
-
-            // We open and elephant moves.
-            for (Valve elephantNext : graph.successors(state.elephantPos)) {
-              newStates.add(new State(state.ourPos, elephantNext, state.open.plus(state.ourPos)));
-            }
-            // We open and elephant opens.
-            ValveSet newOpen = state.open.plus(state.ourPos).plus(state.elephantPos);
-            newStates.add(new State(state.ourPos, state.elephantPos, newOpen));
-            for (State newState : newStates) {
-              nextStates.merge(newState, total + flowRate, Long::max);
-            }
-          });
-
-      // Prune nextStates. For each combination of our position and the elephant's position, look
-      // at all the sets of open valves. If any set contains another set and has at least as high
-      // a total, then the other set can be discarded.
-      // This is obviously quadratic, which is not great. We could improve it somewhat by ordering
-      // the StateAndTotal values by the bitmask of the ValveSet, since if one set contains another
-      // then its bitmask will be numerically greater. We could also skip the pruning at the last
-      // step since we're only going to be looking at the max value anyway. But changing the overall
-      // approach would obviously be much better than fiddling with those details.
-      Map<Position, Set<StateAndTotal>> positionToStates = new HashMap<>();
-      nextStates.forEach(
-          (state, total) ->
-              positionToStates
-                  .computeIfAbsent(new Position(state.ourPos, state.elephantPos), _ -> new HashSet<>())
-                  .add(new StateAndTotal(state, total)));
-      positionToStates.forEach(
-          (position, stateAndTotal) -> {
-            for (var it = stateAndTotal.iterator(); it.hasNext(); ) {
-              var thisOne = it.next();
-              for (var thatOne : stateAndTotal) {
-                if (thisOne != thatOne && thatOne.betterThan(thisOne)) {
-                  it.remove();
-                  break;
-                }
-              }
-            }
-          });
-      // Now reconstruct the new currentStates from the pruned nextStates.
-      currentStates = positionToStates.values().stream()
-          .flatMap(s -> s.stream())
-          .collect(toImmutableMap(StateAndTotal::state, StateAndTotal::total));
-    }
-    long max = currentStates.values().stream().max(Comparator.naturalOrder()).get();
-    System.out.println(STR."Max for \{name} part 2 is \{max}");
-  }
-
-  record State(Valve ourPos, Valve elephantPos, ValveSet open) {}
-
-  record StateAndTotal(State state, long total) {
-    boolean betterThan(StateAndTotal that) {
-      return this.total >= that.total && this.state.open.containsAll(that.state.open);
-    }
-  }
-
-  record Position(Valve ourPos, Valve elephantPos) {}
+  record State(Valve ourPos, ValveSet open) {}
 
   private static final Pattern VALVE_PATTERN = Pattern.compile(
       "Valve (..) has flow rate=(\\d+); tunnels? leads? to valves? (.*)");
@@ -191,14 +130,17 @@ public class Puzzle16 {
 
     // Add the nodes
     Map<String, Valve> map = new TreeMap<>();
-    ImmutableList.Builder<Valve> valves = ImmutableList.builder();
+    ImmutableList.Builder<Valve> nonZeroValves = ImmutableList.builder();
     int valveNumber = 0;
     for (String line : lines) {
       Matcher matcher = VALVE_PATTERN.matcher(line);
       checkArgument(matcher.matches(), line);
-      Valve valve = new Valve(valveNumber++, matcher.group(1), Integer.parseInt(matcher.group(2)));
+      Valve valve = new Valve(valveNumber, matcher.group(1), Integer.parseInt(matcher.group(2)));
       map.put(valve.name, valve);
-      valves.add(valve);
+      if (valve.flowRate > 0) {
+        nonZeroValves.add(valve);
+        valveNumber++;
+      }
       builder.addNode(valve);
     }
 
@@ -214,7 +156,7 @@ public class Puzzle16 {
     }
 
     ImmutableGraph<Valve> graph = builder.build();
-    return new Puzzle16(graph, valves.build());
+    return new Puzzle16(graph, nonZeroValves.build());
   }
 
   record Valve(int valveNumber, String name, int flowRate) {
@@ -223,7 +165,7 @@ public class Puzzle16 {
     }
 
     @Override public boolean equals(Object o) {
-      return o instanceof Valve that && this.valveNumber == that.valveNumber;
+      return o instanceof Valve that && this.valveNumber == that.valveNumber && this.name.equals(that.name);
     }
 
     @Override public int hashCode() {
@@ -250,19 +192,19 @@ public class Puzzle16 {
       return (m == mask) ? this : new ValveSet(m);
     }
 
+    boolean disjoint(ValveSet that) {
+      return (this.mask & that.mask) == 0;
+    }
+
     long flowRate() {
       long total = 0;
       long m = mask;
       while (m != 0) {
         int b = Long.numberOfTrailingZeros(m);
-        total += valves.get(b).flowRate;
+        total += nonZeroValves.get(b).flowRate;
         m &= ~(1L << b);
       }
       return total;
-    }
-
-    boolean containsAll(ValveSet that) {
-      return (that.mask & ~this.mask) == 0;
     }
 
     @Override public boolean equals(Object o) {
