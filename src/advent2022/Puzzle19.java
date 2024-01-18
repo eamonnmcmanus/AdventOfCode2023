e68b3a30fcdd75930b514db78de6fcce32c24c0b
Éamonn McManus <emcmanus@google.com>
2024-01-18 15:46:25 -0800

    Work in progress on Day 19 of 2022.

--- src/advent2022/Puzzle19.java
+++ src/advent2022/Puzzle19.java
@@ -8,6 +8,8 @@ import com.google.common.io.CharStreams;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Callable;
@@ -45,11 +47,90 @@ public class Puzzle19 {
         List<String> lines = CharStreams.readLines(r);
         List<Blueprint> blueprints = lines.stream().map(line -> parseBlueprint(line)).toList();
         System.out.println(blueprints);
+        for (Blueprint blueprint : blueprints) {
+          check(blueprint, 24);
+        }
       }
     }
   }
 
+  private static void check(Blueprint blueprint, int minutes) {
+    List<Status> statuses = List.of(Status.INITIAL);
+    for (int i = 1; i <= minutes; i++) {
+      List<Status> newStatuses = new ArrayList<>();
+      for (Status status : statuses) {
+        Status newStatus = status.next();
+        // Consider doing nothing.
+        addStatus(newStatuses, newStatus);
+        // Consider building an ore robot.
+        if (status.resources.contains(blueprint.oreRobotCost)) {
+          addStatus(
+              newStatuses,
+              new Status(
+                  newStatus.resources.minus(blueprint.oreRobotCost),
+                  newStatus.oreRobots + 1,
+                  newStatus.clayRobots,
+                  newStatus.obsidianRobots,
+                  newStatus.geodeRobots,
+                  newStatus.geodes));
+        }
+        // Consider building a clay robot.
+        if (status.resources.contains(blueprint.clayRobotCost)) {
+          addStatus(
+              newStatuses,
+              new Status(
+                  newStatus.resources.minus(blueprint.clayRobotCost),
+                  newStatus.oreRobots,
+                  newStatus.clayRobots + 1,
+                  newStatus.obsidianRobots,
+                  newStatus.geodeRobots,
+                  newStatus.geodes));
+        }
+        // Consider building an obsidian robot.
+        if (status.resources.contains(blueprint.obsidianRobotCost)) {
+          addStatus(
+              newStatuses,
+              new Status(
+                  newStatus.resources.minus(blueprint.obsidianRobotCost),
+                  newStatus.oreRobots,
+                  newStatus.clayRobots,
+                  newStatus.obsidianRobots + 1,
+                  newStatus.geodeRobots,
+                  newStatus.geodes));
+        }
+        // Consider building a geode robot.
+        if (status.resources.contains(blueprint.geodeRobotCost)) {
+          addStatus(
+              newStatuses,
+              new Status(
+                  newStatus.resources.minus(blueprint.geodeRobotCost),
+                  newStatus.oreRobots,
+                  newStatus.clayRobots,
+                  newStatus.obsidianRobots,
+                  newStatus.geodeRobots + 1,
+                  newStatus.geodes));
+        }
+      }
+      statuses = newStatuses;
+      System.out.println(STR."After minute \{i}, number of statuses is \{statuses.size()}");
+    }
+  }
+
+  private static void addStatus(List<Status> statuses, Status status) {
+    for (Iterator<Status> it = statuses.iterator(); it.hasNext(); ) {
+      Status existing = it.next();
+      if (existing.contains(status)) {
+        return;
+      }
+      if (status.contains(existing)) {
+        it.remove();
+      }
+    }
+    statuses.add(status);
+  }
+
   record Resources(int ore, int clay, int obsidian) {
+    static final Resources ZERO = new Resources(0, 0, 0);
     boolean contains(Resources that) {
       return this.ore >= that.ore && this.clay >= that.clay && this.obsidian >= that.obsidian;
     }
@@ -60,23 +141,37 @@ public class Puzzle19 {
   }
 
   record Blueprint(
-      int id, Resources oreCost, Resources clayCost, Resources obsidianCost, Resources geodeCost) {
-    int evaluate(
-        int remaining, int geodes,
-        int oreRobots, int clayRobots, int obsidianRobots, int geodeRobots,
-        Resources resources) {
-      if (remaining == 0) {
-        return geodes;
-      }
-      int best = geodes;
-      if (resources.contains(oreCost)) {
-        best = max(best,
-            evaluate(
-                remaining - 1, geodes,
-                oreRobots + 1, clayRobots, obsidianRobots, geodeRobots,
-                resources.minus(oreCost)));
-      }
-      throw new AssertionError();
+      int id,
+      Resources oreRobotCost,
+      Resources clayRobotCost,
+      Resources obsidianRobotCost,
+      Resources geodeRobotCost) {
+  }
+
+  record Status(
+      Resources resources,
+      int oreRobots, int clayRobots, int obsidianRobots, int geodeRobots,
+      int geodes) {
+    static final Status INITIAL = new Status(Resources.ZERO, 1, 0, 0, 0, 0);
+
+    Status next() {
+      Resources nextResources = new Resources(
+          resources.ore + oreRobots,
+          resources.clay + clayRobots,
+          resources.obsidian + obsidianRobots);
+      return new Status(
+          nextResources,
+          oreRobots, clayRobots, obsidianRobots, geodeRobots,
+          geodes + geodeRobots);
+    }
+
+    boolean contains(Status that) {
+      return resources.contains(that.resources)
+          && oreRobots >= that.oreRobots
+          && clayRobots >= that.clayRobots
+          && obsidianRobots >= that.obsidianRobots
+          && geodeRobots >= that.geodeRobots
+          && geodes >= that.geodes;
     }
   }
 
