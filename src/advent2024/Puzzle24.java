fa8c04ae023b94a85b7db105a7ba05483b693b72
Éamonn McManus <emcmanus@google.com>
2024-12-24 15:22:26 -0800

    Partial solution for Day 24 of 2024.
    This still involves some manual work. I may try to automate it later.
    But I've had enough for now.

--- src/advent2024/Puzzle24.java
+++ src/advent2024/Puzzle24.java
@@ -7,12 +7,7 @@ import static com.google.common.collect.ImmutableMap.toImmutableMap;
 import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static com.google.common.collect.ImmutableSetMultimap.toImmutableSetMultimap;
 import static java.util.Map.entry;
-import static java.util.stream.Collectors.toMap;
 
-import adventlib.CharGrid;
-import adventlib.CharGrid.Coord;
-import adventlib.Dir;
-import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -23,8 +18,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
@@ -41,6 +36,7 @@ import java.util.stream.Stream;
 /**
  * @author Éamonn McManus
  */
+// PARTIAL SOLUTION for Part 2. This still involved some manual steps.
 public class Puzzle24 {
   private static final String SAMPLE1 =
       """
@@ -128,11 +124,9 @@ public class Puzzle24 {
         ImmutableList<Gate> gates = parseGates(lines.subList(blank + 1, lines.size()));
         ImmutableSetMultimap<String, Gate> wireToInputs =
             gates.stream()
-                .<Map.Entry<String, Gate>>mapMulti(
-                    (gate, consumer) -> {
-                      consumer.accept(entry(gate.inputs.get(0), gate));
-                      consumer.accept(entry(gate.inputs.get(1), gate));
-                    })
+                .flatMap(
+                    gate ->
+                        Stream.of(entry(gate.inputs.get(0), gate), entry(gate.inputs.get(1), gate)))
                 .collect(toImmutableSetMultimap(Map.Entry::getKey, Map.Entry::getValue));
         System.out.printf(
             "For %s, computed number is %d\n", name, compute(wireToInputs, initialWireValues));
@@ -153,9 +147,8 @@ public class Puzzle24 {
       var bit = value.getValue();
       var old = wireValues.put(wire, bit);
       if (old != null) {
-        System.err.printf("More than one value for %s\n", wire);
+        return -1;
       }
-      checkState(old == null);
       for (Gate gate : wireToInputs.get(wire)) {
         Boolean input0 = wireValues.get(gate.inputs.get(0));
         Boolean input1 = wireValues.get(gate.inputs.get(1));
@@ -191,7 +184,160 @@ public class Puzzle24 {
       Stream.concat(X_INPUTS.stream(), Y_INPUTS.stream())
           .collect(toImmutableMap(s -> s, s -> false));
 
+  private sealed interface Node {}
+
+  private record Intermediate(Node a, Op op, Node b) implements Node {
+    @Override
+    public String toString() {
+      List<String> operands = List.of(a.toString(), b.toString());
+      if (operands.get(0).compareTo(operands.get(1)) > 0) {
+        operands = List.of(operands.get(1), operands.get(0));
+      }
+      return "(" + operands.get(0) + " " + op + " " + operands.get(1) + ")";
+    }
+
+    @Override
+    public boolean equals(Object x) {
+      return x instanceof Intermediate that
+          && this.op == that.op
+          && ((this.a.equals(that.a) && this.b.equals(that.b))
+              || (this.a.equals(that.b) && this.b.equals(that.a)));
+    }
+
+    @Override
+    public int hashCode() {
+      return 1001 * op.hashCode() + 31 * (a.hashCode() + b.hashCode());
+    }
+  }
+
+  private record Input(String wire) implements Node {
+    Input(String prefix, int bit) {
+      this(bitToWire(prefix, bit));
+    }
+
+    @Override
+    public String toString() {
+      return wire;
+    }
+  }
+
+  // Work in progress. I figured out what the tree for each output bit should be, and then searched
+  // for subtrees of that tree. The output wire of the largest subtree needs to be swapped with
+  // the output wire of another tree. I worked out which one manually. I think it's probably
+  // possible to complete the automation but I am stopping here for now.
   private static void part2(ImmutableSetMultimap<String, Gate> wireToInputs) {
+    for (int i = 0; i < 4; i++) {
+      System.out.printf("%d: %s\n", i, expected(i));
+    }
+    ImmutableMap<String, String> exchanges =
+        ImmutableMap.of(
+            "z07", "vmv",
+            "vmv", "z07",
+            "z20", "kfm",
+            "kfm", "z20",
+            "hnv", "z28",
+            "z28", "hnv",
+            "tqr", "hth",
+            "hth", "tqr");
+    wireToInputs =
+        wireToInputs.entries().stream()
+            .map(
+                e -> {
+                  Gate gate1 = e.getValue();
+                  String output1 = gate1.output;
+                  String output2 = exchanges.get(output1);
+                  if (output2 == null) {
+                    return e;
+                  } else {
+                    Gate gate2 = new Gate(gate1.inputs, gate1.op, output2);
+                    return entry(e.getKey(), gate2);
+                  }
+                })
+            .collect(toImmutableSetMultimap(Map.Entry::getKey, Map.Entry::getValue));
+    ImmutableMap<String, Gate> outputWireToGate =
+        wireToInputs.values().stream().distinct().collect(toImmutableMap(g -> g.output, g -> g));
+    ImmutableMap<String, Node> outputWireToNode =
+        outputWireToGate.entrySet().stream()
+            .map(e -> entry(e.getKey(), makeNode(e.getValue(), outputWireToGate)))
+            .collect(toImmutableMap(Map.Entry::getKey, Map.Entry::getValue));
+    // This is the manual step: I put in each output bit that had the wrong tree of gates, looked
+    // at the subtrees found below, and worked out what swaps were needed. Kind of laborious.
+    Node expected1 = expected(35);
+    System.out.printf("expected: %s\n", expected1);
+    findSubtrees(expected1, outputWireToNode);
+    for (int z = 0; z < 46; z++) {
+      String wire = bitToWire("z", z);
+      Node expected = expected(z);
+      Node actual = makeNode(outputWireToGate.get(wire), outputWireToGate);
+      if (!expected.equals(actual)) {
+        System.out.printf("Mismatch for %s:\n", wire);
+        System.out.printf("Expected: %s\n", expected);
+        System.out.printf("Actual  : %s\n", actual);
+      }
+    }
+    System.out.println(String.join(",", new TreeSet<>(exchanges.keySet())));
+  }
+
+  private static void findSubtrees(Node expected, ImmutableMap<String, Node> outputWireToNode) {
+    switch (expected) {
+      case Input i -> {
+        return;
+      }
+      case Intermediate(Node a, Op op, Node b) -> {
+        for (var entry : outputWireToNode.entrySet()) {
+          if (entry.getValue().equals(expected)) {
+            System.out.printf("found %s = %s\n", entry.getKey(), expected);
+            return;
+          }
+        }
+        findSubtrees(a, outputWireToNode);
+        findSubtrees(b, outputWireToNode);
+      }
+    }
+  }
+
+  private static Node expected(int bit) {
+    if (bit == 0) {
+      return new Intermediate(new Input("x00"), Op.XOR, new Input("y00"));
+    }
+    Node addBit = new Intermediate(new Input("x", bit), Op.XOR, new Input("y", bit));
+    Node carry = carry(bit);
+    if (bit == 45) {
+      return carry;
+    } else {
+      return new Intermediate(carry, Op.XOR, addBit);
+    }
+  }
+
+  private static Node carry(int intoBit) {
+    if (intoBit == 1) {
+      return new Intermediate(new Input("x00"), Op.AND, new Input("y00"));
+    }
+    int prevBit = intoBit - 1;
+    Node prevCarry = carry(prevBit);
+    Node prevWasZero = new Intermediate(new Input("x", prevBit), Op.XOR, new Input("y", prevBit));
+    Node carryFromPrev = new Intermediate(prevCarry, Op.AND, prevWasZero);
+    Node prevBothOnes = new Intermediate(new Input("x", prevBit), Op.AND, new Input("y", prevBit));
+    return new Intermediate(carryFromPrev, Op.OR, prevBothOnes);
+  }
+
+  private static Node makeNode(Gate gate, ImmutableMap<String, Gate> wireToOutput) {
+    List<Node> inputs = new ArrayList<>();
+    for (String wire : gate.inputs) {
+      Gate inGate = wireToOutput.get(wire);
+      Node in = (inGate == null) ? new Input(wire) : makeNode(inGate, wireToOutput);
+      inputs.add(in);
+    }
+    return new Intermediate(inputs.get(0), gate.op, inputs.get(1));
+  }
+
+  // My idea here was to check each bit separately to see whether addition of just that bit
+  // works correctly. That should limit the number of gates whose outputs we need to consider
+  // swapping. Unfortunately it doesn't limit it enough.
+  // I also tried swapping each pair of gates and seeing whether that reduced the number of wrong
+  // additions, but that isn't sound because a swap can reduce that number coincidentally without
+  // being a correct swap.
+  private static void oldPart2(ImmutableSetMultimap<String, Gate> wireToInputs) {
     checkState(!wireToInputs.containsKey("x45"));
     checkState(!wireToInputs.containsKey("y45"));
     Set<String> suspectWires = new TreeSet<>();
@@ -241,10 +387,16 @@ public class Puzzle24 {
     }
     System.out.printf(
         "%d suspect gates from suspect result wires: %s\n", suspectGates.size(), suspectGates);
-    SetMultimap<String, Gate> modifiedWireToInputs = HashMultimap.create(wireToInputs);
+    for (Gate gate : new LinkedHashSet<>(wireToInputs.values())) {
+      if (new LinkedHashSet<>(gate.inputs).equals(Set.of("x07", "y07"))) {
+        System.out.println(gate);
+      }
+    }
+    ImmutableSetMultimap<String, Gate> modifiedWireToInputs = wireToInputs;
     long currentIncorrect = incorrectCount(modifiedWireToInputs);
     outer:
     while (currentIncorrect > 0) {
+      System.out.printf("@@@ looping with currentIncorrect=%d\n", currentIncorrect);
       ImmutableMap<String, Gate> wireToOutput =
           modifiedWireToInputs.values().stream()
               .distinct()
@@ -252,25 +404,26 @@ public class Puzzle24 {
       ImmutableList<String> wires = wireToOutput.keySet().asList();
       for (int i = 0; i < wires.size(); i++) {
         String wire1 = wires.get(i);
+        System.out.printf("%d of %d (%s)\n", i, wires.size(), wires.get(i));
         for (int j = i + 1; j < wires.size(); j++) {
           String wire2 = wires.get(j);
-          SetMultimap<String, Gate> newWireToInputs = HashMultimap.create(modifiedWireToInputs);
           Gate gate1 = wireToOutput.get(wire1);
           Gate gate2 = wireToOutput.get(wire2);
-          newWireToInputs.remove(wire1, gate1);
-          newWireToInputs.remove(wire2, gate2);
           Gate newGate1 = new Gate(gate1.inputs, gate1.op, gate2.output);
           Gate newGate2 = new Gate(gate2.inputs, gate2.op, gate1.output);
-          newWireToInputs.put(wire1, newGate1);
-          newWireToInputs.put(wire2, newGate2);
-          long newIncorrect;
-          try {
-            newIncorrect = incorrectCount(newWireToInputs);
-          } catch (IllegalStateException e) {
-            System.out.printf("Fail for %s and %s, was %s and %s, now %s and %s\n", wire1, wire2, gate1, gate2, newGate1, newGate2);
-            throw e;
-          }
+          ImmutableSetMultimap<String, Gate> newWireToInputs =
+              modifiedWireToInputs.entries().stream()
+                  .map(
+                      e ->
+                          entry(
+                              e.getKey(),
+                              e.getValue() == gate1
+                                  ? newGate1
+                                  : e.getValue() == gate2 ? newGate2 : e.getValue()))
+                  .collect(toImmutableSetMultimap(Map.Entry::getKey, Map.Entry::getValue));
+          long newIncorrect = incorrectCount(newWireToInputs);
           if (newIncorrect < currentIncorrect) {
+            System.out.printf("@@@ swapped %s and %s\n", newGate1, newGate2);
             currentIncorrect = newIncorrect;
             modifiedWireToInputs = newWireToInputs;
             continue outer;
